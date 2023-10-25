f351e660bb07cc50a72e7669b3f458352821dfe2
RAlbrechtsen <r.albrechtsen93@gmail.com>
2023-10-25 09:58:58 +0200

    changed readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # github-test-project
+
+# Dette er en ny linie i vores readme fil !!!!!!!!
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Test</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi corporis, eveniet delectus fuga, sequi facilis, consequuntur ut earum adipisci praesentium? Doloribus magni animi voluptate.</p>
     </div>
   );
 }
